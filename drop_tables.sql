8ee0d6fa347e1358ca1b48f2e2de02ea09c9143b
Kevin Lapeyre <doclap77@gmail.com>
2017-11-05 15:14:57 -0800

    Broke into two files for create and drop

---
+++ drop_tables.sql
@@ -0,0 +1,25 @@
+drop table saleitems;
+
+drop table orderitems;
+
+drop table storeitems;
+
+drop table beers;
+
+drop table wines;
+
+drop table items;
+
+drop table reportamounts;
+
+drop table reports;
+
+drop table orders;
+
+drop table store_sales;
+
+drop table employees;
+
+drop table stores;
+
+commit;
