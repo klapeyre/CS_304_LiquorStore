a943b66563ab1d2e97228f590a8971ba98a7841c
vanderkooi <timavanderkooi@gmail.com>
2017-11-09 12:44:43 -0800

    Search stock stores query results in table
    * Can search by SKU and product name

--- src/View/SearchStock.java
+++ src/View/SearchStock.java
@@ -3,18 +3,20 @@ package View;
 import SQL.SQLSearchStock;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
-import java.awt.BorderLayout;
 
 public class SearchStock extends JFrame {
     private JPanel searchStockPanel;
     private JTextField nameField;
     private JTextField skuField;
-    private JLabel skuLabel;
     private JButton skuSearchButton;
+    private JLabel searchBySKULabel;
+    private JLabel searchByNameLabel;
     private JButton nameButton;
+    private JScrollPane resultsPane;
     private SQLSearchStock search;
 
     public SearchStock() {
@@ -24,11 +26,7 @@ public class SearchStock extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 int sku = Integer.parseInt(skuField.getText());
                 try {
-                    JTable table = new JTable(search.buildResultsTableModel(search.selectBySKU(sku)));
-                    getContentPane().add(new JScrollPane(table), BorderLayout.CENTER); // omly POC
-                    // TODO: embed results in current screen, not pop-up table like this
-                    pack();
-                    setVisible(true);
+                    setTableInScrollPane(new JTable(search.buildResultsTableModel(search.selectBySKU(sku))));
                 } catch (SQLException e1) {
                     e1.printStackTrace();
                 }
@@ -40,11 +38,7 @@ public class SearchStock extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 String name = nameField.getText();
                 try {
-                    JTable table = new JTable(search.buildResultsTableModel(search.selectByName(name)));
-                    getContentPane().add(new JScrollPane(table), BorderLayout.CENTER); // omly POC
-                    // TODO: embed results in current screen, not pop-up table like this
-                    pack();
-                    setVisible(true);
+                    setTableInScrollPane(new JTable(search.buildResultsTableModel(search.selectByName(name))));
                 } catch (SQLException e1) {
                     e1.printStackTrace();
                 }
@@ -55,4 +49,10 @@ public class SearchStock extends JFrame {
     public JPanel getSearchStockPanel() {
         return searchStockPanel;
     }
+
+    private void setTableInScrollPane(JTable table) {
+        table.setPreferredScrollableViewportSize(new Dimension(400, 100));
+        resultsPane.setViewportView(table);
+    }
+
 }
