454f8ecbb30bf45e9b0031cd01ae0d175e65e9af
vanderkooi <timavanderkooi@gmail.com>
2017-11-09 23:16:55 -0800

    [WIP] Can search by type, subtype, and store
    * Need to include all wines, all beers
    * Need to include names and descriptions

--- src/SQL/SQLSearchStock.java
+++ src/SQL/SQLSearchStock.java
@@ -1,28 +1,109 @@
 package SQL;
 
+import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.sql.*;
 import java.util.Vector;
 
 public class SQLSearchStock {
+    private final String ALL_STORES = "All Stores";
+    private final String BEERS = "Beers";
     Connection con;
 
     public SQLSearchStock() {
         con = DatabaseConnection.getConnection();
     }
 
-    public ResultSet selectBySKU(int sku) throws SQLException {
-        PreparedStatement ps = con.prepareStatement(
-                "SELECT i.sku, i.name, i.price, i.description " + "FROM ITEMS i " + "WHERE i.sku = ?");
+    public ResultSet selectBySKU(int sku, boolean tax, String storeName) throws SQLException {
+        PreparedStatement ps;
+        if (tax) {
+            ps = con.prepareStatement(
+                    "SELECT i.sku, s.name as storeName, i.name, i.price + i.tax as price, i.description, si.stock_quantity "
+                            + "FROM ITEMS i, STORES s, STOREITEMS si "
+                            + "WHERE i.sku = si.sku AND s.store_id = si.store_id AND i.sku = ? AND s.name LIKE ? ");
+        } else {
+            ps = con.prepareStatement(
+                    "SELECT i.sku, s.name as storeName, i.name, i.price, i.description, si.stock_quantity "
+                            + "FROM ITEMS i, STORES s, STOREITEMS si "
+                            + "WHERE i.sku = si.sku AND s.store_id = si.store_id AND i.sku = ? AND s.name LIKE ? ");
+        }
         ps.setInt(1, sku);
+        setStoreVariable(ps, storeName);
         return ps.executeQuery();
     }
 
-    public ResultSet selectByName(String name) throws SQLException {
-        PreparedStatement ps = con.prepareStatement(
-                "SELECT i.sku, i.name, i.price, i.description " + "FROM ITEMS i " + "WHERE i.name LIKE ?");
+    public ResultSet selectByName(String name, boolean tax, String storeName) throws SQLException {
+        PreparedStatement ps;
+        if (tax) {
+            ps = con.prepareStatement(
+                    "SELECT i.sku, s.name as storeName, i.name, i.price + i.tax as price, i.description, si.stock_quantity "
+                            + "FROM ITEMS i, STORES s, STOREITEMS si "
+                            + "WHERE i.sku = si.sku AND s.store_id = si.store_id AND i.name LIKE ? AND s.name LIKE ? ");
+        } else {
+            ps = con.prepareStatement(
+                    "SELECT i.sku, s.name as storeName, i.name, i.price, i.description, si.stock_quantity "
+                            + "FROM ITEMS i, STORES s, STOREITEMS si "
+                            + "WHERE i.sku = si.sku AND s.store_id = si.store_id AND i.name LIKE ? AND s.name LIKE ? ");
+        }
+
         ps.setString(1, "%" + name + "%");
+        setStoreVariable(ps, storeName);
         return ps.executeQuery();
+    }
+
+    public ResultSet selectBySubType(String subType, boolean beer, String storeName) throws SQLException{
+        PreparedStatement ps;
+        if (beer) {
+            ps = con.prepareStatement(
+                    "SELECT b.sku, s.name as storeName, b.company, si.stock_quantity "
+                        + "FROM BEERS b, STORES s, STOREITEMS si "
+                        + "WHERE b.sku = si.sku AND s.store_id = si.store_id AND b.type = ? AND s.name LIKE ? ");
+        } else {
+            ps = con.prepareStatement(
+                    "SELECT w.sku, s.name as storeName, w.company, si.stock_quantity "
+                            + "FROM WINES w, STORES s, STOREITEMS si "
+                            + "WHERE w.sku = si.sku AND s.store_id = si.store_id AND w.type = ? AND s.name LIKE ? ");
+        }
+        ps.setString(1, subType);
+        setStoreVariable(ps, storeName);
+        return ps.executeQuery();
+    }
+
+    public Vector<String> selectByStore() throws SQLException {
+        Vector<String> storeNames = new Vector<String>();
+        storeNames.add(ALL_STORES);
+        PreparedStatement ps = con.prepareStatement("SELECT s.name " + "FROM STORES s ");
+
+        ResultSet results = ps.executeQuery();
+        while (results.next()) {
+            storeNames.add(results.getString(1));
+        }
+
+        return storeNames;
+    }
+
+    public Vector<String> selectByType(String type) throws SQLException {
+        PreparedStatement ps;
+        Vector<String> typeNames = new Vector<>();
+        if (type.equals(BEERS)) {
+            ps = con.prepareStatement("SELECT DISTINCT b.type " + "FROM BEERS b ");
+        } else {
+            ps = con.prepareStatement("SELECT DISTINCT w.type " + "FROM WINES w ");
+        }
+        ResultSet results = ps.executeQuery();
+        while (results.next()) {
+            typeNames.add(results.getString(1));
+        }
+
+        return typeNames;
+    }
+
+    public void setStoreVariable(PreparedStatement ps, String storeName) throws SQLException {
+        if (storeName.equals(ALL_STORES)) {
+            ps.setString(2,  "%");
+        } else {
+            ps.setString(2, storeName);
+        }
     }
 
     public DefaultTableModel buildResultsTableModel(ResultSet results) throws SQLException {
--- src/View/SearchStock.java
+++ src/View/SearchStock.java
@@ -7,6 +7,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
+import java.util.Vector;
 
 public class SearchStock extends JFrame {
     private JPanel searchStockPanel;
@@ -17,16 +18,36 @@ public class SearchStock extends JFrame {
     private JLabel searchByNameLabel;
     private JButton nameButton;
     private JScrollPane resultsPane;
+    private JComboBox storeNames;
+    private JLabel taxLabel;
+    private JRadioButton beforeTaxRadioButton;
+    private JRadioButton afterTaxRadioButton;
+    private JRadioButton beerRadionButton;
+    private JRadioButton wineRadioButton;
+    private JButton typeButton;
+    private JComboBox subTypes;
     private SQLSearchStock search;
 
     public SearchStock() {
         search = new SQLSearchStock();
+        setAndGroupButtons(beforeTaxRadioButton, afterTaxRadioButton); // allows user to only select one button at a time
+        setAndGroupButtons(beerRadionButton, wineRadioButton);
+        populateStoreDropDown();
+        populateSubTypeDropDown("Beers");
+
         skuSearchButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 int sku = Integer.parseInt(skuField.getText());
+                String store = (String) storeNames.getSelectedItem();
                 try {
-                    setTableInScrollPane(new JTable(search.buildResultsTableModel(search.selectBySKU(sku))));
+                    if (beforeTaxRadioButton.isSelected()) {
+                        setTableInScrollPane(new JTable
+                                (search.buildResultsTableModel(search.selectBySKU(sku, false, store))));
+                    } else {
+                        setTableInScrollPane(new JTable
+                                (search.buildResultsTableModel(search.selectBySKU(sku, true, store))));
+                    }
                 } catch (SQLException e1) {
                     e1.printStackTrace();
                 }
@@ -37,13 +58,91 @@ public class SearchStock extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String name = nameField.getText();
+                String store = (String) storeNames.getSelectedItem();
                 try {
-                    setTableInScrollPane(new JTable(search.buildResultsTableModel(search.selectByName(name))));
+                    if (beforeTaxRadioButton.isSelected()) {
+                        setTableInScrollPane(new JTable
+                                (search.buildResultsTableModel(search.selectByName(name, false, store))));
+                    } else {
+                        setTableInScrollPane(new JTable
+                                (search.buildResultsTableModel(search.selectByName(name, true, store))));
+
+                    }
                 } catch (SQLException e1) {
                     e1.printStackTrace();
                 }
             }
         });
+
+        typeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String subType = (String) subTypes.getSelectedItem();
+                String store = (String) storeNames.getSelectedItem();
+                try {
+                    if (beerRadionButton.isSelected()) {
+                        setTableInScrollPane(new JTable
+                                (search.buildResultsTableModel(search.selectBySubType(subType, true, store))));
+                    } else {
+                        setTableInScrollPane(new JTable
+                                (search.buildResultsTableModel(search.selectBySubType(subType, false, store))));
+                    }
+                } catch (SQLException e1) {
+                    e1.printStackTrace();
+                }
+
+            }
+        });
+
+        beerRadionButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (beerRadionButton.isSelected()) {
+                    populateSubTypeDropDown(beerRadionButton.getText().trim());
+                }
+            }
+        });
+
+        wineRadioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (wineRadioButton.isSelected()) {
+                    populateSubTypeDropDown(wineRadioButton.getText().trim());
+                }
+            }
+        });
+    }
+
+    private void setAndGroupButtons(JRadioButton brother, JRadioButton sister) {
+        ButtonGroup group = new ButtonGroup();
+        group.add(brother);
+        group.add(sister);
+        brother.setSelected(true);
+    }
+
+    private void populateStoreDropDown() {
+        try {
+            Vector<String> stores = search.selectByStore();
+            for (int i = 0; i < stores.size(); i++) {
+                storeNames.addItem(stores.get(i));
+            }
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void populateSubTypeDropDown(String type) {
+        subTypes.removeAllItems();
+        try {
+            Vector<String> types = search.selectByType(type);
+            for (int i = 0; i < types.size(); i++) {
+                subTypes.addItem(types.get(i));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public JPanel getSearchStockPanel() {
