a58f7ac1ee3480c04f9bcaaa40769211bdfb738e
Kevin Lapeyre <doclap77@gmail.com>
2017-11-14 22:29:45 -0800

    Final version of ViewSales, should be okay

--- src/App.java
+++ src/App.java
@@ -6,7 +6,7 @@ public class App {
 
     public static void main(String[] args) {
         DatabaseConnection database = new DatabaseConnection();
-        database.establishConnection("stub", "stub");
+        database.establishConnection("ora_i4n0b", "a17677089");
         JFrame frame = new JFrame("Cool App");
         frame.setContentPane(new MainScreen().getPanelMain());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
