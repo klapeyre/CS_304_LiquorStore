bce1cdc0926036889a136e3ce39299f10119675b
vanderkooi <timavanderkooi@gmail.com>
2017-11-15 21:22:19 -0800

    Fixed date bug by advancing end date one day
    * Couldn't use Kevin's fix because of the OR clause
    * Using OR nullified all of the previous AND conditions

--- src/SQL/SQLViewOrders.java
+++ src/SQL/SQLViewOrders.java
@@ -1,6 +1,9 @@
 package SQL;
 
 import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class SQLViewOrders {
     private final Connection con;
@@ -9,16 +12,31 @@ public class SQLViewOrders {
         con = DatabaseConnection.getConnection();
     }
 
-    public ResultSet searchBySKUAndDate(int sku, Date fromDate, Date toDate) throws SQLException {
+    public ResultSet searchBySKUAndDate(int sku, Date startDate, Date endDate) throws SQLException {
+        Date inclusiveEndDate = advanceEndDateOneDay(endDate);
         PreparedStatement ps = con.prepareStatement(
                 "SELECT oi.order_number, i.sku, i.name, oi.quantity, i.price as individual_price, i.price * oi.quantity as total_price "
                         + "FROM ORDERS o, ORDERITEMS oi, ITEMS i "
-                        + "WHERE oi.sku = ? AND oi.sku = i.sku AND oi.order_number = o.order_number AND o.time_date_placed BETWEEN ? AND ?"
+                        + "WHERE oi.sku = ? AND oi.sku = i.sku AND oi.order_number = o.order_number AND o.time_date_placed BETWEEN ? AND ? "
         );
         ps.setInt(1, sku);
-        ps.setDate(2, fromDate);
-        ps.setDate(3, toDate);
+        ps.setDate(2, startDate);
+        ps.setDate(3, inclusiveEndDate);
+
         return ps.executeQuery();
+    }
+
+    private Date advanceEndDateOneDay(Date endDate) {
+        String dt = endDate.toString();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Calendar c = Calendar.getInstance();
+        try {
+            c.setTime(sdf.parse(dt));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        c.add(Calendar.DATE, 1);  // number of days to add
+        return Date.valueOf(sdf.format(c.getTime()));
     }
 
     public ResultSet searchByOrderNumber(int orderNumber) throws SQLException {
