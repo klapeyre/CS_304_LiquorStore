5e75c7f88ba6f1c788f632ff1f89477165dd5edc
vanderkooi <timavanderkooi@gmail.com>
2017-11-19 19:06:12 -0800

    Change name to Liquor Store App

--- src/App.java
+++ src/App.java
@@ -7,7 +7,7 @@ public class App {
     public static void main(String[] args) {
         DatabaseConnection database = new DatabaseConnection();
         database.establishConnection("stub", "stub");
-        JFrame frame = new JFrame("Cool App");
+        JFrame frame = new JFrame("Liquor Store App");
         frame.setContentPane(new MainScreen().getPanelMain());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
